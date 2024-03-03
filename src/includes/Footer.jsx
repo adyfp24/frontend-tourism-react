6962fd0a3c8393f103c754b1b342db9d83baa0ce
adyfp24 <adyfp24@gmail.com>
2024-03-03 14:37:57 +0700

    feat(footer): adding mobile responsive

--- src/includes/Footer.jsx
+++ src/includes/Footer.jsx
@@ -7,42 +7,42 @@ function Footer() {
   return (
     <div>
       <footer id='tips' className='mt-14 h-full font-sans'>
-          <div className='text-white h-72 px-16 pt-14 flex justify-between w-full bg-custom-500'>
-              <div className='font-bold'>logo</div>
+          <div className='text-white h-72 px-8 sm:px-16 pt-14 flex justify-between w-full bg-custom-500'>
+              <div className='font-bold hidden sm:block'>logo</div>
               <div>
                 <h1 className='font-bold pb-4'>Malang</h1>
-                <ul className='font-extralight'>
+                <ul className='font-extralight text-xs'>
                   <li className='pb-2'>Tentang</li>
                   <li className='pb-2'>Destinasi</li>
                   <li className='pb-2'>Blog</li>
                   <li className='pb-2'>FAQ</li>
                 </ul>
               </div>
-              <div>
+              <div className='ml-11 sm:ml-0'>
                 <h1 className='font-bold pb-4'>Layanan</h1>
-                <ul className='font-extralight'>
+                <ul className='font-extralight text-xs'>
                   <li className='pb-2'>Kontak Kami</li>
                   <li className='pb-2'>Syarat dan Ketentuan</li> 
                   <li className='pb-2'>Keamanan Privasi</li>
                   <li className='pb-2'>Peta</li>
                 </ul>
               </div>
-              <div>
+              <div className='ml-6 sm:ml-0'>
                 <h1 className='font-bold pb-4 '>Media Sosial</h1>
                 <ul className='flex'>
                   <li className='hover:cursor-pointer'>
-                    <img src={fb} alt="" />
+                    <img className='' src={fb} alt="" />
                   </li>
                   <li className='pl-3 hover:cursor-pointer'>
-                    <img src={twit} alt="" />
+                    <img className='' src={twit} alt="" />
                   </li>
                   <li className='pl-3 pr-4 hover:cursor-pointer'>
-                    <img src={ig} alt="" />
+                    <img className='' src={ig} alt="" />
                   </li>
                 </ul>
               </div>
           </div>
-          <div className='w-full py-6 px-16 font-thin text-white bg-emerald-950'>Copyright 2024 Malang. All Rights Reserved</div>
+          <div className='w-full py-6 px-8 sm:px-16 font-thin sm:text-base text-sm text-white bg-emerald-950'>Copyright 2024 Malangkuy. All Rights Reserved</div>
       </footer>
     </div>
   )
